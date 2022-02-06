ec6cd8bef10727bb88147fc6bc0755bc378d4d27
Nikos Roussos <nikos@roussos.cc>
2022-02-06 13:00:33 +0200

    Add CI config for linting

---
+++ Changelog.md
@@ -0,0 +1,11 @@
+# Changelog
+
+This document tracks all notable changes to radicale-imap, introduced on each release.
+
+## 0.2.0 - 2021-02-06
+
+- Make plugin Radicale 3.x compatible
+
+## 0.1.0 - 2018-04-18
+
+- Initial release
--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from setuptools import find_packages, setup
 
 
+def readme_md():
+    """Return contents of README.md"""
+    return open("README.md").read()
+
+
+def changelog_md():
+    """Return contents of Changelog.md"""
+    return open("Changelog.md").read()
+
+
 setup(
     name='radicale-imap',
-    version='0.1.2',
+    version='0.2.0',
     author='Nikos Roussos',
     author_email='nikos@roussos.cc',
     url='https://gitlab.com/comzeradd/radicale-imap/',
     description='Radicale IMAP authentication plugin',
+    long_description_content_type="text/markdown",
+    long_description=readme_md() + "\n\n" + changelog_md(),
     include_package_data=True,
     zip_safe=False,
     packages=find_packages(),
